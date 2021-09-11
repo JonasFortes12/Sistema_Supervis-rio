3722818dc67ce2a9d5035a493d65c0bdbeb25362
Jonas Fortes <jonascforte@hotmail.com>
2021-09-11 11:51:27 -0300

    grafic images nos moldes indianos

--- aplication.py
+++ aplication.py
@@ -23,6 +23,7 @@ def grafic1Load():
             title='Corrente')
         
         g1.plot(time, data)
+        
 
 def grafic2Load():
         
@@ -87,45 +88,70 @@ class Aplication():
     def grafic1(self):
         
         #criando uma figura:
-        figure1 = plt.figure(figsize=(8, 4), dpi=70) #Define as dimensões da figura
-        figure1.add_subplot(111)
-        canva = FigureCanvasTkAgg(figure1, root) #Instancia a figura dentro da janela  
+        figure1 = plt.Figure()
+        ax = figure1.add_subplot(111)
+        ax.set_title('Serial Data')
+        ax.set_xlabel('Sample')
+        ax.set_ylabel('Voltage')
+        ax.set_xlim(0,100)
+        ax.set_ylim(-0.5,6)
+        lines1 = ax.plot([],[])[0]
+        
+        canva = FigureCanvasTkAgg(figure1, master = root) #Instancia a figura dentro da janela  
         canva.get_tk_widget().place(relx=0.025, rely=0.05,
                                     relwidth=0.30, relheight=0.30)# Tamanho e posição na tela
+        canva.draw()
 
     def grafic2(self):
         
         #criando uma figura:
-        figure2 = plt.figure(figsize=(8, 4), dpi=70) #Define as dimensões da figura
-        figure2.add_subplot(111)
-        canva = FigureCanvasTkAgg(figure2, root) #Instancia a figura dentro da janela  
+        figure2 = plt.Figure()
+        ax = figure2.add_subplot(111)
+        ax.set_title('Serial Data')
+        ax.set_xlabel('Sample')
+        ax.set_ylabel('Voltage')
+        ax.set_xlim(0,100)
+        ax.set_ylim(-0.5,6)
+        lines2 = ax.plot([],[])[0]
+
+        canva = FigureCanvasTkAgg(figure2, master = root) #Instancia a figura dentro da janela  
         canva.get_tk_widget().place(relx=0.350, rely=0.05,
                                     relwidth=0.30, relheight=0.30)# Tamnaho e posição na tela
+        canva.draw()
 
     def grafic3(self):
         
         #criando uma figura:
-        figure3 = plt.figure(figsize=(8, 4), dpi=70) #Define as dimensões da figura
-        figure3.add_subplot(111)
-        canva = FigureCanvasTkAgg(figure3, root) #Instancia a figura dentro da janela  
+        figure3 = plt.Figure()
+        ax = figure3.add_subplot(111)
+        ax.set_title('Serial Data')
+        ax.set_xlabel('Sample')
+        ax.set_ylabel('Voltage')
+        ax.set_xlim(0,100)
+        ax.set_ylim(-0.5,6)
+        lines3 = ax.plot([],[])[0]
+
+        canva = FigureCanvasTkAgg(figure3, master = root) #Instancia a figura dentro da janela  
         canva.get_tk_widget().place(relx=0.675, rely=0.05,
                                     relwidth=0.30, relheight=0.30)# Tamnaho e posição na tela
+        canva.draw()
 
     def frameScreen(self):
-        self.frame1 = Frame(self.root, bd= 4, bg = "red")
+        self.frame1 = Frame(self.root, bd= 4, bg = "blue")
         self.frame1.place(relx=0.05, rely=0.6, relwidth=0.90, relheight=0.20)
 
     def buttonsFrame(self):
-        #botão limpar
-        self.bt1 = Button(self.frame1, text="Carregar Vetor", command=initGrafics)
+        #Start
+        self.bt1 = Button(self.frame1, text="Start", font = ('calbiri',12),
+                command = lambda: initGrafics())
         self.bt1.place(relx=0.2, rely=0.5, relwidth=0.1, relheight=0.15, width=20, height=20)
 
-        #botão Buscar
-        self.bt2 = Button(self.frame1, text="Botão 2")
+        #Stop
+        self.bt2 = Button(self.frame1, text="Stop", font = ('calbiri',12))
         self.bt2.place(relx=0.4, rely=0.5, relwidth=0.1, relheight=0.15, width=20, height=20)
         
         #botão Novo
-        self.bt3 = Button(self.frame1, text="Botão 3")
+        self.bt3 = Button(self.frame1, text="Botão 3", font = ('calbiri',12))
         self.bt3.place(relx=0.6, rely=0.5, relwidth=0.1, relheight=0.15, width=20, height=20)
 
 
