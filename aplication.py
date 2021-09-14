457bf8673cc1abbf07aba46a732893c1ca6534dd
Jonas Fortes <jonascforte@hotmail.com>
2021-09-14 15:15:12 -0300

    Teste 2 - Gráfico dinâmico dentro da jeanela sem bug

--- aplication.py
+++ aplication.py
@@ -7,62 +7,16 @@ import numpy as np
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg 
 root = Tk()
 
-def grafic1Load():
-        
-        #criando uma figura:
-        figure1 = plt.figure(figsize=(8, 4), dpi=70) #Define as dimensões da figura
-        g1 = figure1.add_subplot(111) #Coloca a figura dentro da variavel grafico
+graficOn = False
 
-        canva = FigureCanvasTkAgg(figure1, root) #Instancia a figura dentro da janela  
-        canva.get_tk_widget().place(relx=0.025, rely=0.05,
-                                    relwidth=0.30, relheight=0.30)# Tamnaho e posição na tela
-        
-        data = loadingDataCurrent()
-        time = range(1, len(data)+1)
-        g1.set(xlabel='Tempo (s)', ylabel='Posição (m)',
-            title='Corrente')
-        
-        g1.plot(time, data)
-        
 
-def grafic2Load():
-        
-        #criando uma figura:
-        figure2 = plt.figure(figsize=(8, 4), dpi=70) #Define as dimensões da figura
-        g2 = figure2.add_subplot(111) #Coloca a figura dentro da variavel grafico
+def On():
+    global graficOn 
+    graficOn = True
 
-        canva = FigureCanvasTkAgg(figure2, root) #Instancia a figura dentro da janela  
-        canva.get_tk_widget().place(relx=0.350, rely=0.05,
-                                    relwidth=0.30, relheight=0.30)# Tamnaho e posição na tela
-        
-        data = loadingDataTension()
-        x = range(1, len(data)+1)
 
-        g2.set(xlabel='Tempo (s)', ylabel='Posição (m)',
-            title='Tensão')
-        g2.plot(x, data)
 
-def grafic3Load():
-        
-        #criando uma figura:
-        figure3 = plt.figure(figsize=(8, 4), dpi=70) #Define as dimensões da figura
-        g3 = figure3.add_subplot(111) #Coloca a figura dentro da variavel grafico
 
-        canva = FigureCanvasTkAgg(figure3, root) #Instancia a figura dentro da janela  
-        canva.get_tk_widget().place(relx=0.675, rely=0.05,
-                                    relwidth=0.30, relheight=0.30)# Tamnaho e posição na tela
-        
-        data = loadingDataInput()
-        x = range(1, len(data)+1)
-
-        g3.set(xlabel='Tempo (s)', ylabel='Posição (m)',
-            title='Engrada')
-        g3.plot(x, data)
-
-def initGrafics():
-    grafic1Load()
-    grafic2Load()
-    grafic3Load()
 
 # ------------------------------------------------------------------------------------------
 
@@ -91,15 +45,13 @@ class Aplication():
         figure1 = plt.Figure()
         ax = figure1.add_subplot(111)
         ax.set_title('Serial Data')
-        ax.set_xlabel('Sample')
-        ax.set_ylabel('Voltage')
         ax.set_xlim(0,100)
         ax.set_ylim(-0.5,6)
         lines1 = ax.plot([],[])[0]
-        
+
         canva = FigureCanvasTkAgg(figure1, master = root) #Instancia a figura dentro da janela  
         canva.get_tk_widget().place(relx=0.025, rely=0.05,
-                                    relwidth=0.30, relheight=0.30)# Tamanho e posição na tela
+                                    relwidth=0.30, relheight=0.30)# Tamnaho e posição na tela
         canva.draw()
 
     def grafic2(self):
@@ -108,8 +60,6 @@ class Aplication():
         figure2 = plt.Figure()
         ax = figure2.add_subplot(111)
         ax.set_title('Serial Data')
-        ax.set_xlabel('Sample')
-        ax.set_ylabel('Voltage')
         ax.set_xlim(0,100)
         ax.set_ylim(-0.5,6)
         lines2 = ax.plot([],[])[0]
@@ -125,8 +75,6 @@ class Aplication():
         figure3 = plt.Figure()
         ax = figure3.add_subplot(111)
         ax.set_title('Serial Data')
-        ax.set_xlabel('Sample')
-        ax.set_ylabel('Voltage')
         ax.set_xlim(0,100)
         ax.set_ylim(-0.5,6)
         lines3 = ax.plot([],[])[0]
@@ -143,7 +91,7 @@ class Aplication():
     def buttonsFrame(self):
         #Start
         self.bt1 = Button(self.frame1, text="Start", font = ('calbiri',12),
-                command = lambda: initGrafics())
+                command = lambda: On())
         self.bt1.place(relx=0.2, rely=0.5, relwidth=0.1, relheight=0.15, width=20, height=20)
 
         #Stop
