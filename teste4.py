61de6adcea6ef565a425f88d42178067ea9e520b
Jonas Fortes <jonascforte@hotmail.com>
2021-09-30 17:30:43 -0300

    Adicionando botão reiniciar

--- teste4.py
+++ teste4.py
@@ -14,15 +14,16 @@ root.resizable(True, True) #Responsividade
 root.geometry("1200x700")
 
 
+dataTension = loadingDataTension()
+dataCurrent = loadingDataCurrent()
+dataInput = loadingDataInput()
 #variáveis do intervalo dos graficos
 a = 0
-b = 500
+b = len(dataTension)
 
 #criando uma figura 1:
 figure1 = plt.figure(figsize=(10, 6), dpi=70) #Define as dimensões da figura
-
 g1 = figure1.add_subplot(111) #Coloca a figura dentro da variavel grafico
-
 g1.set_title('Serial Data')
 g1.set_xlabel('Sample')
 g1.set_ylabel('Tension')
@@ -67,10 +68,6 @@ canva3.get_tk_widget().place(relx=0.675, rely=0.2,
 canva3.draw()
 
 
-dataTension = loadingDataTension()
-dataCurrent = loadingDataCurrent()
-dataInput = loadingDataInput()
-
 i = 1 #eixo x
 j = 0 #eixo y
 on = False
@@ -109,23 +106,22 @@ def update_data():
 
 def plot_data():
     if(on == True):
-        global xTension, yTension, xCurrent, yCurrent, xInput, yInput
         update_data()
-        if(True):
-            lines1.set_xdata(xTension)
-            lines1.set_ydata(yTension)
+        
+        lines1.set_xdata(xTension)
+        lines1.set_ydata(yTension)
             
-            lines2.set_xdata(xCurrent)
-            lines2.set_ydata(yCurrent)
+        lines2.set_xdata(xCurrent)
+        lines2.set_ydata(yCurrent)
             
-            lines3.set_xdata(xInput)
-            lines3.set_ydata(yInput)
+        lines3.set_xdata(xInput)
+        lines3.set_ydata(yInput)
             
-            canva1.draw()
-            canva2.draw()
-            canva3.draw()
+        canva1.draw()
+        canva2.draw()
+        canva3.draw()
             
-            root.after(1,plot_data)
+        root.after(1,plot_data)
     else:
         root.after(1,plot_data)
 
@@ -133,9 +129,29 @@ def onOf():
     global on
     on = not on
 
+def restart():
+    global xTension, xCurrent, xInput, yCurrent, yTension, yInput, i, j, a, b, on
+    xTension = []
+    xCurrent = []
+    xInput = []
+    yTension = []
+    yCurrent = []
+    yInput = []
+    i = 1 #eixo x
+    j = 0 #eixo y
+    a = 0
+    b = len(dataTension)
+    g1.set_xlim(a, b)
+    g2.set_xlim(a, b)
+    g3.set_xlim(a, b)
+
 root.update()
-start = tkinter.Button(root, text = "Start/Stop", font = ('calbiri',12),command = lambda: onOf())
+start = tkinter.Button(root, text = "Start/Stop", font = ('calbiri',15),command = lambda: onOf())
 start.place(relx = 0.44, rely = 0.7, relwidth=0.1, relheight=0.05)
+
+root.update()
+start = tkinter.Button(root, text = "Reiniciar", font = ('calbiri',15),command = lambda: restart())
+start.place(relx = 0.44, rely = 0.8, relwidth=0.1, relheight=0.05)
 
 
 root.after(1,plot_data)
